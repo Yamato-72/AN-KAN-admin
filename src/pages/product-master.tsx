a39834ee38a8aac18791d4a6e7fa5142de90e8d8
Yamato-72 <toshihiro.wada8010@yamato-signage.com>
2026-01-27 17:27:05 +0900

    サイドバー項目名変更　製品マスタ→マスタ管理

--- src/pages/product-master.tsx
+++ src/pages/product-master.tsx
@@ -7,7 +7,7 @@ export default function ProductMaster() {
     <div style={{ height: "calc(100vh - 60px)", width: "100%" }}>
       <IframeWithLoading
         src="https://master-edit-app-47228502594.europe-west1.run.app/masters"
-        title="製品マスタ"
+        title="マスタ管理"
         height="100%"
       />
     </div>
