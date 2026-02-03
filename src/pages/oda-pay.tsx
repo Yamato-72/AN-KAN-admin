84bc8a360df4ca2041f0f95870c8271749128aa1
Yamato-72 <toshihiro.wada8010@yamato-signage.com>
2026-02-03 11:38:17 +0900

    Oda-payリンクの変更

--- src/pages/oda-pay.tsx
+++ src/pages/oda-pay.tsx
@@ -6,7 +6,7 @@ export default function ProductMaster() {
   return (
     <div style={{ height: "calc(100vh - 60px)", width: "100%" }}>
       <IframeWithLoading
-        src="https://oda-pay-47228502594.europe-west1.run.app/dashboard"
+        src="https://oda-pay-47228502594.europe-west1.run.app/index"
         title="支払管理"
         height="100%"
       />
